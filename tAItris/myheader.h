cde1a0bfd6269a9e7757b69b0f6c6cfdc30a81f5
hwi-middle <gnlwnd5084@gmail.com>
2020-12-25 10:35:59 +0900

    코딩 스탠다드를 따르기 위해 include guard 관련 코드 변경

--- tAItris/myheader.h
+++ tAItris/myheader.h
@@ -1,12 +1,11 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
+#pragma once
 
 #include <iostream>
-#include <vector>
+#include <vector>	
 #include <string>
-#include <conio.h> //방향키 검출에 필요
+#include <conio.h>	//방향키 검출에 필요
 
-enum class eTitleActions
+enum class eTitleActions	//타이틀 화면에서 선택할 수 있는 동작들
 {
 	START,
 	HELP,
@@ -14,16 +13,21 @@ enum class eTitleActions
 	EXIT
 };
 
-enum class eInputKey {
+enum class eInputKey	//유효한 입력 키들
+{
 	ARROW_LEFT,
 	ARROW_RIGHT,
 	ARROW_UP,
 	ARROW_DOWN,
 	ENTER,
+	Z,
+	X,
+	C,
+	V,
 	INVALID
 };
 
-enum class eTetrominoType
+enum class eTetrominoType	//테트로미노의 타입들
 {
 	I,
 	O,
@@ -36,5 +40,3 @@ enum class eTetrominoType
 
 eTitleActions ShowTitle(void);	//타이틀 화면 출력 및 게임 종료 선택 여부 return
 eInputKey GetArrowKey(void);	//방향키 입력 받아 해당 키에 해당하는 enum return
-
-#endif // MYHEADER_H
